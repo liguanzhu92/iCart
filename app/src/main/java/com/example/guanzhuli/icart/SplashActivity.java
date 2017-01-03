7a98eace0f8c6d0dd4726531d3335f203ae5198c
Jiabei Xu <Jiabei Xu>
2017-01-02 22:35:29 -0600

    make splash full screen, fixed wording on signin

--- app/src/main/java/com/example/guanzhuli/icart/SplashActivity.java
+++ app/src/main/java/com/example/guanzhuli/icart/SplashActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -14,6 +16,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         mButtonClose = (ImageView) findViewById(R.id.splash_close);
         mButtonClose.setOnClickListener(new View.OnClickListener() {
